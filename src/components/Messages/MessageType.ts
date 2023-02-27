ff193f3d4bbe25a2c9b443b96b5e4c8cc2c5f110
Binh2 <hgqbinh2002@gmail.com>
2023-02-27 16:40:59 +0700

    Differentiate between FirestoreMessageType and MessageType (normal vanilla JS)

--- src/components/Messages/MessageType.ts
+++ src/components/Messages/MessageType.ts
@@ -1,15 +1,29 @@
 import { DocumentData, Timestamp } from "firebase/firestore"
 
-export type MessageType = {
+export type FirestoreMessageType = {
   message: string,
   time: Timestamp,
   isReceived: boolean,
   from: string,
 }
-export function toMessageType(data: DocumentData) {
+export type MessageType = {
+  message: string,
+  time: Date,
+  isReceived: boolean,
+  from: string,
+}
+export function toFirestoreMessageType(data: DocumentData) {
   return {
     message: data.message,
     time: data.time,
+    isReceived: data.isReceived,
+    from: data.from
+  }
+}
+export function toMessageType(data: FirestoreMessageType) {
+  return {
+    message: data.message,
+    time: data.time.toDate(),
     isReceived: data.isReceived,
     from: data.from
   }
--- src/components/Messages/useMessage.ts
+++ src/components/Messages/useMessage.ts
@@ -1,19 +1,19 @@
 import { getFirestore, collection, onSnapshot, query, DocumentData, QuerySnapshot, limit, orderBy } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { MessageType, toMessageType } from "./MessageType";
+import { FirestoreMessageType, MessageType, toFirestoreMessageType, toMessageType } from "./MessageType";
 
 export function useMessageFromGroupId(props: {groupId: string}) {
-  const [ message, setMessage ] = useState<MessageType | null>(null);
+  const [ message, setMessage ] = useState<FirestoreMessageType | null>(null);
   useEffect(() => {
     return onSnapshot(query(collection(getFirestore(), "groups", props.groupId, "messages"), 
       orderBy("time"), limit(1)), 
       (querySnapshot: QuerySnapshot<DocumentData>) => {
         if (querySnapshot.docs.length > 0)
-          setMessage(toMessageType(querySnapshot.docs[0].data))
+          setMessage(toFirestoreMessageType(querySnapshot.docs[0].data))
       }
     )
   })
   return {
-    message,
+    message: message != null ? toMessageType(message) : null
   }
 }
