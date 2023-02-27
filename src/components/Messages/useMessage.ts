6cfa2af588d57fa942ae414086c6bf55edd97ba3
Binh2 <hgqbinh2002@gmail.com>
2023-02-27 17:02:44 +0700

    refactor message -> firestoreMessages in useMessage.ts

--- src/components/Messages/useMessage.ts
+++ src/components/Messages/useMessage.ts
@@ -2,18 +2,18 @@ import { getFirestore, collection, onSnapshot, query, DocumentData, QuerySnapsho
 import { useEffect, useState } from "react";
 import { FirestoreMessageType, MessageType, toFirestoreMessageType, toMessageType } from "./MessageType";
 
-export function useMessageFromGroupId(props: {groupId: string}) {
-  const [ message, setMessage ] = useState<FirestoreMessageType | null>(null);
+export function useMessage(props: {groupId: string}) {
+  const [ firestoreMessage, setFirestoreMessage ] = useState<FirestoreMessageType | null>(null);
   useEffect(() => {
     return onSnapshot(query(collection(getFirestore(), "groups", props.groupId, "messages"), 
       orderBy("time"), limit(1)), 
       (querySnapshot: QuerySnapshot<DocumentData>) => {
         if (querySnapshot.docs.length > 0)
-          setMessage(toFirestoreMessageType(querySnapshot.docs[0].data))
+          setFirestoreMessage(toFirestoreMessageType(querySnapshot.docs[0].data))
       }
     )
   })
   return {
-    message: message != null ? toMessageType(message) : null
+    message: firestoreMessage != null ? toMessageType(firestoreMessage) : null
   }
 }
